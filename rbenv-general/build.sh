0394e8f20435d2a00333bf01fbe70c4492b9640a
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-16 15:40:42 -0500

    Updated rbenv-general

--- rbenv-general/build.sh
+++ rbenv-general/build.sh
@@ -2,12 +2,15 @@
 
 DOCKER_IMAGE='jhsu802701/debian-jessie-rbenv-general'
 
+sh delete.sh
+
 DATE=`date +%Y_%m%d_%H%M_%S`
 DIR_ROOT=$(dirname $PWD)
 DIR_LOG=$DIR_ROOT/log
 mkdir -p $DIR_LOG
 
 docker build -t $DOCKER_IMAGE . 2>&1 | tee $DIR_LOG/build-$DATE.txt
-echo '------------------------------'
-echo "docker run -i -t $DOCKER_IMAGE"
-docker run -i -t $DOCKER_IMAGE
+
+echo '*******************************'
+echo "Finished building $DOCKER_IMAGE"
+echo '*******************************'
