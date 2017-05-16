0c7b0e31140018e6b5e7801ba2cafd2922181617
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-16 14:23:48 -0500

    Updated rbenv-jekyll

--- rbenv-jekyll/build.sh
+++ rbenv-jekyll/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DOCKER_IMAGE='jhsu802701/debian-jessie-rbenv-general'
+DOCKER_IMAGE='jhsu802701/debian-jessie-rbenv-jekyll'
+
+sh delete.sh
 
 DATE=`date +%Y_%m%d_%H%M_%S`
 DIR_ROOT=$(dirname $PWD)
@@ -8,6 +10,7 @@ DIR_LOG=$DIR_ROOT/log
 mkdir -p $DIR_LOG
 
 docker build -t $DOCKER_IMAGE . 2>&1 | tee $DIR_LOG/build-$DATE.txt
-echo '------------------------------'
-echo "docker run -i -t $DOCKER_IMAGE"
-docker run -i -t $DOCKER_IMAGE
+
+echo '*******************************'
+echo "Finished building $DOCKER_IMAGE"
+echo '*******************************'
